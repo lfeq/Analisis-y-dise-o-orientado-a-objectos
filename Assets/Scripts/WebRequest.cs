5fa0df532f300be6cea2f04135798905e420a72c
lfeq <panchocosa@gmail.com>
2023-08-23 12:14:19 -0600

    Tarea

--- Assets/Scripts/WebRequest.cs
+++ Assets/Scripts/WebRequest.cs
@@ -1,55 +1,36 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
-using UnityEngine.PlayerLoop;
 
-public class WebRequest : MonoBehaviour
-{
-    void Start()
-    {
+public class Example : MonoBehaviour {
+
+    private void Start() {
         // A correct website page.
+        StartCoroutine(GetRequest("https://www.example.com"));
 
         // A non-existing page.
-        //StartCoroutine(GetRequest("https://error.html"));
+        StartCoroutine(GetRequest("https://error.html"));
     }
 
-    void Update()
-    {
-        StartCoroutine(GetRequest("http://10.2.0.51:3000/"));
-        StartCoroutine(GetRequest("http://10.2.0.51:3000/"));
-        StartCoroutine(GetRequest("http://10.2.0.51:3000/"));
-        StartCoroutine(GetRequest("http://10.2.0.51:3000/"));
-        StartCoroutine(GetRequest("http://10.2.0.51:3000/"));
-        StartCoroutine(GetRequest("http://10.2.0.51:3000/"));
-        StartCoroutine(GetRequest("http://10.2.0.51:3000/"));
-        StartCoroutine(GetRequest("http://10.2.0.51:3000/"));
-        StartCoroutine(GetRequest("http://10.2.0.51:3000/"));
-        StartCoroutine(GetRequest("http://10.2.0.51:3000/"));
-        StartCoroutine(GetRequest("http://10.2.0.51:3000/"));
-        StartCoroutine(GetRequest("http://10.2.0.51:3000/"));
-        StartCoroutine(GetRequest("http://10.2.0.51:3000/"));
-        StartCoroutine(GetRequest("http://10.2.0.51:3000/"));
-        StartCoroutine(GetRequest("http://10.2.0.51:3000/"));
-    }
-    
-
-    IEnumerator GetRequest(string uri)
-    {
-        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
-        {
+    private IEnumerator GetRequest(string uri) {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri)) {
             // Request and wait for the desired page.
             yield return webRequest.SendWebRequest();
 
             string[] pages = uri.Split('/');
             int page = pages.Length - 1;
 
-            if (webRequest.isNetworkError)
-            {
-                Debug.Log(pages[page] + ": Error: " + webRequest.error);
-            }
-            else
-            {
-                Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+            switch (webRequest.result) {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                    break;
             }
         }
     }
